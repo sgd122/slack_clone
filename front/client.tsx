6972855b7b7690ce90f7d47e3312c3a98aca5367
성기동 <sgd0947@gmail.com>
2021-07-06 15:20:07 +0900

    feat: babel과 webpack 설정

---
+++ front/client.tsx
@@ -0,0 +1,9 @@
+import App from './layout/App';
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<App />, document.querySelector('#app'));
+
+// pages - 서비스 페이지
+// components - 짜잘 컴포넌트
+// layouts - 공통 레이아웃
