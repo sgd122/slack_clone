e2f3bca6b9f474cb21bea12031dd698f5e367bc8
성기동 <sgd0947@gmail.com>
2021-07-06 16:35:57 +0900

    feat: webpack dev server 세팅

--- front/webpack.config.ts
+++ front/webpack.config.ts
@@ -1,8 +1,9 @@
+import * as webpack from 'webpack';
+
 // import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-// import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-// import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import path from 'path';
-import webpack from 'webpack';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -41,14 +42,11 @@ const config: webpack.Configuration = {
             '@babel/preset-react',
             '@babel/preset-typescript',
           ],
-          // env: {
-          //   development: {
-          //     plugins: [['@emotion', { sourceMap: true }], require.resolve('react-refresh/babel')],
-          //   },
-          //   production: {
-          //     plugins: ['@emotion'],
-          //   },
-          // },
+          env: {
+            development: {
+              plugins: [require.resolve('react-refresh/babel')],
+            },
+          },
         },
         exclude: path.join(__dirname, 'node_modules'),
       },
@@ -59,12 +57,12 @@ const config: webpack.Configuration = {
     ],
   },
   plugins: [
-    // new ForkTsCheckerWebpackPlugin({
-    //   async: false,
-    //   // eslint: {
-    //   //   files: "./src/**/*",
-    //   // },
-    // }),
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      // eslint: {
+      //   files: "./src/**/*",
+      // },
+    }),
     new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
   ],
   output: {
@@ -72,22 +70,22 @@ const config: webpack.Configuration = {
     filename: '[name].js',
     publicPath: '/dist/',
   },
-  // devServer: {
-  //   historyApiFallback: true, // react router
-  //   port: 3090,
-  //   publicPath: '/dist/',
-  //   proxy: {
-  //     '/api/': {
-  //       target: 'http://localhost:3095',
-  //       changeOrigin: true,
-  //     },
-  //   },
-  // },
+  devServer: {
+    historyApiFallback: true, // react router
+    port: 3090,
+    publicPath: '/dist/',
+    // proxy: {
+    //   '/api/': {
+    //     target: 'http://localhost:3095',
+    //     changeOrigin: true,
+    //   },
+    // },
+  },
 };
 
 if (isDevelopment && config.plugins) {
-  // config.plugins.push(new webpack.HotModuleReplacementPlugin());
-  // config.plugins.push(new ReactRefreshWebpackPlugin());
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  config.plugins.push(new ReactRefreshWebpackPlugin());
   // config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: true }));
 }
 if (!isDevelopment && config.plugins) {
