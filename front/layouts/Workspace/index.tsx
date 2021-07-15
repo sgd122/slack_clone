21968b3af72d7ddaa96af076ebeb9107b0b9071f
성기동 <sgd0947@gmail.com>
2021-07-15 09:15:44 +0900

    feat: workspace 명칭 나올 수 있도록 변경

--- front/layouts/Workspace/index.tsx
+++ front/layouts/Workspace/index.tsx
@@ -139,11 +139,13 @@ const Workspace: VFC = () => {
           <AddButton onClick={onClickCreateWorkspace}>+</AddButton>
         </Workspaces>
         <Channels>
-          <WorkspaceName onClick={toggleWorkspaceModal}>Slack Clone</WorkspaceName>
+          <WorkspaceName onClick={toggleWorkspaceModal}>
+            {userData?.Workspaces.find((v) => v.url === workspace)?.name}
+          </WorkspaceName>
           <MenuScroll>
             <Menu show={showWorkspaceModal} onCloseModal={toggleWorkspaceModal} style={{ top: 95, left: 80 }}>
               <WorkspaceModal>
-                <h2>Slack Clone</h2>
+                <h2>{userData?.Workspaces.find((v) => v.url === workspace)?.name}</h2>
                 <button onClick={onClickInviteWorkspace}>워크스페이스에 사용자 초대</button>
                 <button onClick={onClickAddChannel}>채널 만들기</button>
                 <button onClick={onLogout}>로그아웃</button>
