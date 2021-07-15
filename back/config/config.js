29efbd6e545e153de8836ee8c0784fa795c00a3a
성기동 <sgd0947@gmail.com>
2021-07-15 10:17:24 +0900

    docs: production .env 설정

--- back/config/config.js
+++ back/config/config.js
@@ -17,9 +17,9 @@ module.exports = {
   },
   "production": {
     "username": "root",
-    "password": process.env.MYSQL_PASSWORD,
-    "database": "sleact",
-    "host": "127.0.0.1",
+    "password": process.env.MYSQL_PRODUCTION_PASSWORD,
+    "database": "slack",
+    "host": "doul.c6twjfaqgp0c.ap-northeast-2.rds.amazonaws.com",
     "dialect": "mysql"
   }
 }
