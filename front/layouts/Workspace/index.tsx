e26857defedec79249a2ca1facbaf9470df26ab1
성기동 <sgd0947@gmail.com>
2021-07-15 09:43:12 +0900

    fix: find 관련 객체 에러 수정

--- front/layouts/Workspace/index.tsx
+++ front/layouts/Workspace/index.tsx
@@ -140,12 +140,12 @@ const Workspace: VFC = () => {
         </Workspaces>
         <Channels>
           <WorkspaceName onClick={toggleWorkspaceModal}>
-            {userData?.Workspaces.find((v) => v.url === workspace)?.name}
+            {userData?.Workspaces?.find((v) => v.url === workspace)?.name}
           </WorkspaceName>
           <MenuScroll>
             <Menu show={showWorkspaceModal} onCloseModal={toggleWorkspaceModal} style={{ top: 95, left: 80 }}>
               <WorkspaceModal>
-                <h2>{userData?.Workspaces.find((v) => v.url === workspace)?.name}</h2>
+                <h2>{userData?.Workspaces?.find((v) => v.url === workspace)?.name}</h2>
                 <button onClick={onClickInviteWorkspace}>워크스페이스에 사용자 초대</button>
                 <button onClick={onClickAddChannel}>채널 만들기</button>
                 <button onClick={onLogout}>로그아웃</button>
